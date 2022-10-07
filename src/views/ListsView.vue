14c8e7f08f554d709f02f831d21e2b569c0f442d
Christopher Robinson <chrispaul_robinson@hotmail.com>
2022-10-07 13:08:15 -0500

    Add alert messages

--- src/views/ListsView.vue
+++ src/views/ListsView.vue
@@ -13,6 +13,7 @@ export default defineComponent({
       shopLists: [] as ShopList[],
       db: {} as IDBDatabase,
       ready: false,
+      invalidList: false,
     };
   },
 
@@ -29,6 +30,13 @@ export default defineComponent({
 
   methods: {
     async addNewList(name: string) {
+      if (this.shopLists.some((list) => list.name === name)) {
+        this.invalidList = true;
+        return;
+      }
+
+      this.invalidList = false;
+
       let newList: ShopList = { name, items: [] };
 
       await idb.saveList(newList);
@@ -87,6 +95,25 @@ export default defineComponent({
     <div class="header-area">
       <h2>Lists</h2>
       <hr />
+      <div
+        v-if="invalidList"
+        class="alert alert-warning d-flex align-items-center"
+        role="alert"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-exclamation-triangle-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+          />
+        </svg>
+        <div>A list with that name already exists</div>
+      </div>
     </div>
 
     <div class="list-form accordion-item">
@@ -155,6 +182,18 @@ main {
 
 div {
   margin: 1.5rem 0;
+}
+
+div.alert {
+  margin: 0;
+}
+
+div.alert > div {
+  margin: 0 0;
+}
+
+.alert > svg {
+  margin: 0 0.4rem;
 }
 
 div.header-area {
