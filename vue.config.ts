4f2b71feb178328a2c87011841bb30f27c17f3cf
Christopher Robinson <chrispaul_robinson@hotmail.com>
2022-10-10 19:47:27 -0500

    Config changes

--- vue.config.ts
+++ vue.config.ts
@@ -2,5 +2,13 @@ module.exports = {
   pwa: {
     name: "Pelican",
     themeColor: "#ffd100",
+    iconPaths: {
+      faviconSVG: "img/icons/favicon.svg",
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/msapplication-icon-144x144.png",
+    },
   },
 };
