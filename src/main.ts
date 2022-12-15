603c4638c7bc49701165fe7a3a106837d31761d0
Christopher Robinson <chrispaul_robinson@hotmail.com>
2022-12-15 14:12:29 -0500

    Customize bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/custom_bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
